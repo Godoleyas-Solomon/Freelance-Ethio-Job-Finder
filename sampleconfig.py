fa964098bd3ab80e0911130fa9d03eefe2bb56f5
BuildTools <itsmepapy@gmail.com>
2023-07-29 15:13:05 +0300

    'sampleconfig added'

---
+++ sampleconfig.py
@@ -0,0 +1,6 @@
+api_id = 1234
+api_hash = 'abcd'
+channel = 'freelance_ethio'
+dump = '-1234'
+cookie = 'abcd'
+prompt = 'Check the following job application and if it is related to software engineering(web development, app development and so on) reply with the message 1, if it is not reply with a 0. And please make sure to not write anything else other than 1/0.'
